b340c0af4c96db8eabcf16987386bdb5ff78097e
Your Name <you@example.com>
2023-03-07 15:50:18 +0100

    Milestone 0 e 1

---
+++ js/main.js
@@ -0,0 +1,65 @@
+"use strict";
+
+//Milestone 0  e 1 
+
+const team = [];
+
+const Ceo = {
+    nome: "Wayne Barnett",
+    ruolo: "Founder e CEO",
+    img: "wayne-barnett-founder-ceo.jpg"
+}
+console.log(Ceo)
+
+const Editor = {
+    nome: "Angela Caroll",
+    ruolo: "Chief Editor",
+    img: "angela-caroll-chief-editor.jpg"
+}
+console.log(Editor)
+
+const OManager = {
+    nome: "Walter Gordon",
+    ruolo: "Office Manager",
+    img: "walter-gordon-office-manager.jpg"
+}
+console.log(OManager)
+
+const SMManager = {
+    nome: "Angela Lopez",
+    ruolo: "Social Media Manager",
+    img: "angela-lopez-social-media-manager.jpg"
+}
+console.log(SMManager)
+
+const Developer = {
+    nome: "Scott Estrada",
+    ruolo: "Developer",
+    img: "scott-estrada-developer.jpg"
+}
+console.log(Developer)
+
+const Designer = {
+    nome: "Barbara Ramos",
+    ruolo: "Graphic Designer",
+    img: "barbara-ramos-graphic-designer.jpg"
+}
+console.log(Designer)
+
+team.push(Ceo, Editor, OManager, SMManager, Developer, Designer)
+
+console.log(team)
+
+
+
+
+
+
+
+
+/*Wayne Barnett	Founder & CEO	wayne-barnett-founder-ceo.jpg
+Angela Caroll	Chief Editor	angela-caroll-chief-editor.jpg
+Walter Gordon	Office Manager	walter-gordon-office-manager.jpg
+Angela Lopez	Social Media Manager	angela-lopez-social-media-manager.jpg
+Scott Estrada	Developer	scott-estrada-developer.jpg
+Barbara Ramos	Graphic Designer	barbara-ramos-graphic-designer.jpg*/
